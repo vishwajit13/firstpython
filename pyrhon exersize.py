dabf6e8d0d1efb3688a61dab90ec5a88573da010
firstpyhon <patilvishwajit707@gmail.com>
2025-03-13 07:23:36 +0530

    written new code

--- pyrhon exersize.py
+++ pyrhon exersize.py
@@ -1,4 +1,10 @@
-birth_year=int(input("enter ur birth year :"))
+'''birth_year=int(input("enter ur birth year :"))
 current_year=int(input("enter current year:"))
 age=(current_year -  birth_year)
-print('your age is:',age)
+print('your age is:',age)'''
+
+f_name=input("enter your first name= ")
+m_name=input("enter your middle name= ")
+l_name=input("enter your last name= ")
+name=f_name+m_name+l_name
+print("your name is",name)
