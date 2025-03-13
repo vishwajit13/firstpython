2624524d588e3e005994f6347797a7a91fed9d55
firstpyhon <patilvishwajit707@gmail.com>
2025-03-13 08:31:44 +0530

    written code

--- pyrhon exersize.py
+++ pyrhon exersize.py
@@ -1,10 +1,38 @@
 '''birth_year=int(input("enter ur birth year :"))
 current_year=int(input("enter current year:"))
 age=(current_year -  birth_year)
-print('your age is:',age)'''
+print('your age is:',age)
 
-f_name=input("enter your first name= ")
+ f_name=input("enter your first name= ")
 m_name=input("enter your middle name= ")
 l_name=input("enter your last name= ")
 name=f_name+m_name+l_name
 print("your name is",name)
+
+
+f_long=92
+f_width=48.8
+area=f_long * f_width
+print("total area of that foootboll ground is;",area)
+
+
+potato_chips=9
+one_packet=1.49
+given_aaamo=20
+shop_ammo=9*1.49
+ammo_return=given_aaamo-shop_ammo
+print("returned ammont is",ammo_return)
+
+
+lenghth_bath_sf=5.5**2
+per_suare_feet=500
+total_cost=lenghth_bath_sf*per_suare_feet
+print("total coast to replace all styles",total_cost)'''
+
+num=17
+print("number in binary",format(num,'b'))
+
+
+
+
+
